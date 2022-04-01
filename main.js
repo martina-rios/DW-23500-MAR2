8d0f02bc79b0ddd69df81a187a73cda5aa3aac51
Mar <martina1asec@gmail.com>
2022-04-01 01:54:36 -0300

    cambios js

---
+++ main.js
@@ -0,0 +1,8 @@
+let descargarJuego = prompt("Hola! Deseas descargar el juego?");
+if (descargarJuego == "si" || descargarJuego == "SI"){
+    prompt("Para Android, iOS o Nintendo?");
+}
+//despues quise hacer que aparezca otro prompt para elegir la plataforma y a continuacion que apareza el link correspondiente, pero no logre hacerlo :(
+else if (descargarJuego == "no" || descargarJuego == "NO"){
+    alert("okay! de todas formas te invitamos a visitar nuestro sitio web :)");
+}
